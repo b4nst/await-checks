c8d11fb28ef572bd636503b8bb359113b5e9723a
Bastien Arata <bastien.arata@d2si.io>
2019-11-05 17:11:19 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from '@actions/core'
+import { wait } from './wait'
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const names = core.getInput('names').split(',')
+    const token = core.getInput('github_token')
+    const includeQueue = core.getInput('include_queue')
+    const waitMs = core.getInput('wait_ms')
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
+    await wait(
+      token,
+      names.length > 0 ? names : [undefined],
+      includeQueue === 'true',
+      parseInt(waitMs, 10)
+    )
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
--- src/wait.ts
+++ src/wait.ts
@@ -1,10 +1,46 @@
-export function wait(milliseconds: number) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds)) { 
-            throw new Error('milleseconds not a number'); 
-        }
+import github from '@actions/github'
+import core from '@actions/core'
+import * as _ from 'lodash'
 
-        setTimeout(() => resolve("done!"), milliseconds)
-    });
+const context = github.context
+
+type CheckStatus = 'queued' | 'in_progress' | 'completed'
+
+export async function wait(
+  ghToken: string,
+  names: Array<string | undefined>,
+  includeQueued: boolean,
+  waitMs: number
+) {
+  const octokit = new github.GitHub(ghToken)
+
+  while (true) {
+    const requests = _.flatMap(names, name =>
+      includeQueued
+        ? [
+            getChecks(octokit, name, 'completed'),
+            getChecks(octokit, name, 'queued')
+          ]
+        : getChecks(octokit, name, 'completed')
+    )
+
+    const res = await Promise.all(requests)
+    const awaiting = _.sum(res)
+    if (awaiting > 0) {
+      console.log(`Awaiting ${awaiting} jobs to complete...`)
+      await new Promise(resolve => setTimeout(resolve, waitMs))
+    } else break
+  }
 }
-  
+
+const getChecks = async (
+  octo: github.GitHub,
+  check_name?: string,
+  status?: CheckStatus
+) =>
+  (await octo.checks.listForRef({
+    ...context.repo,
+    ref: context.ref,
+    check_name,
+    status
+  })).data.total_count
