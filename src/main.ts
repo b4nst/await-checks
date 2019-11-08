93db3b86a15405701f6c9e2cb0b528d57e196584
Bastien Arata <bastien.arata@d2si.io>
2019-11-08 12:19:24 +0100

    refactor main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { wait } from './wait'
 
 async function run() {
   try {
-    const names = core.getInput('names').split(',')
+    const name = core.getInput('name_pattern')
     const token = core.getInput('github_token')
     const includeQueue = core.getInput('include_queue')
     const waitMs = core.getInput('wait_ms')
 
     await wait(
       token,
-      names.length > 0 ? names : [undefined],
+      new RegExp(name),
       includeQueue === 'true',
       parseInt(waitMs, 10)
     )
